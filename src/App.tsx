7e525841314be6579659853eef889e198b4cee6e
Felipe Oliveira <felipe.oliveira1496@gmail.com>
2021-10-27 10:51:53 -0300

    room code button created and Room

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import { Home } from "./pages/Home";
-import { NewRoom } from "./pages/Newroom";
+import { Room } from './pages/Room';
 
-import { AuthContextProvider } from './contexts/AuthContext';
-
+import { AuthContextProvider } from './contexts/AuthContext'
+import { NewRoom } from './pages/NewRoom';
 
 
 function App() {
-
-
   return (
     <BrowserRouter>
       <AuthContextProvider>
-        <Route path='/' exact component={Home} />
-        <Route path='/rooms/new' component={NewRoom} />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/rooms/new" component={NewRoom} />
+          <Route path="/rooms/:id" component={Room} />
+        </Switch>
       </AuthContextProvider>
     </BrowserRouter>
   );
